bde60f7e707f388e56d32d318fb14464a2de7b5b
Alex Kunin <alexkunin@gmail.com>
2024-04-15 09:12:06 +0300

    add basic SPA skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet, MatToolbar, MatSidenavModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
