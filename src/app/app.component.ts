adeeca47aa6d7cb217d3495ca9ec0419cab43fc2
Alex Kunin <alexkunin@gmail.com>
2024-04-15 14:51:47 +0300

    add date selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,45 @@ import { CurrentTimeComponent } from '@/shared/current-time';
 import { GlobalToolbarWidgetDirective, GlobalToolbarWidgetsComponent } from '@/shared/global-toolbar';
 import { NextAppointmentComponent } from '@/shared/next-appointment';
 import { TotalsComponent } from '@/shared/totals';
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatCalendar } from '@angular/material/datepicker';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbar } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, MatToolbar, MatSidenavModule, GlobalToolbarWidgetsComponent, GlobalToolbarWidgetDirective, CurrentTimeComponent, TotalsComponent, NextAppointmentComponent ],
+  imports: [ RouterOutlet, MatToolbar, MatSidenavModule, GlobalToolbarWidgetsComponent, GlobalToolbarWidgetDirective, CurrentTimeComponent, TotalsComponent, NextAppointmentComponent, MatCalendar, AsyncPipe ],
+  providers:[
+    provideNativeDateAdapter(),
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  readonly #route = inject(ActivatedRoute);
+  readonly #dateParam$ = this.#route.params.pipe(map(params => params['date']));
+  readonly date$ = this.#dateParam$.pipe(map(date => {
+    const matches = /^(\d{4})-(\d{2})-(\d{2})$/.exec(date);
+    if (matches) {
+      return new Date(+matches[1], +matches[2] - 1, +matches[3]);
+    } else {
+      return new Date();
+    }
+  }));
+  readonly #router = inject(Router);
+
   title = 'calendar';
+
+  redirect(date: Date | null) {
+    let url = '/day';
+    if (date) {
+      url += '/' + date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0');
+    }
+    this.#router.navigateByUrl(url);
+  }
 }
