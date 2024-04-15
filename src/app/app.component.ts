bd71d80d1457e5eeca82f0c23437520ac2403716
Alex Kunin <alexkunin@gmail.com>
2024-04-15 14:26:18 +0300

    add widget with next appointment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { CurrentTimeComponent } from '@/shared/current-time';
 import { GlobalToolbarWidgetDirective, GlobalToolbarWidgetsComponent } from '@/shared/global-toolbar';
+import { NextAppointmentComponent } from '@/shared/next-appointment';
 import { TotalsComponent } from '@/shared/totals';
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -9,7 +10,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, MatToolbar, MatSidenavModule, GlobalToolbarWidgetsComponent, GlobalToolbarWidgetDirective, CurrentTimeComponent, TotalsComponent ],
+  imports: [ RouterOutlet, MatToolbar, MatSidenavModule, GlobalToolbarWidgetsComponent, GlobalToolbarWidgetDirective, CurrentTimeComponent, TotalsComponent, NextAppointmentComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
